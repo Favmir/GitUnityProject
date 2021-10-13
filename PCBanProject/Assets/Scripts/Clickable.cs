88f2be852d7e37f77bf518e0240b2964c492575f
Favmir <37527099+Favmir@users.noreply.github.com>
2021-10-13 18:57:07 +0900

    Clickable Cat Demo

---
+++ PCBanProject/Assets/Scripts/Clickable.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+
+    public GameObject Scoreboard;
+    public int AddAmount = 1;
+
+    Vector3 touchedPosition;
+    Vector2 size;
+    public Sprite idleSprite;
+    public Sprite clickedSprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;
+        size = GetComponent<SpriteRenderer>().bounds.size;
+    }
+	
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchedPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                touchedPosition.z = 0f;
+
+                if (Vector3.Distance(touchedPosition, transform.position) < size.x)
+                    gameObject.GetComponent<SpriteRenderer>().sprite = clickedSprite;
+                
+
+
+            }
+            if (touch.phase == TouchPhase.Ended)
+                gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;
+        }
+        
+    }
+}
